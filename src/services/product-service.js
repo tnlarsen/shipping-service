aba6d8fa2e0fa9774cd93b168b3c38c6f9445b16
Thorbjørn Larsen <tnl@dk.ibm.com>
2019-01-15 12:28:49 +0100

    Completed step 18

--- src/services/product-service.js
+++ src/services/product-service.js
@@ -1,12 +1,18 @@
-// src/services/product-service.js
 var axios = require('axios')
 
 module.exports = {
-    getProductWeight: async function (productId) {
-        return axios
-            .get('https://mycluster.icp:8899/products/' + productId)
-            .then(response => {
-                return response.data.weightLB
-            })
-    }
+  getProductWeight: async function (productId) {
+    return axios
+      .get('https://mycluster.icp:8899/products/' + productId)
+      .then(response => {
+        if (response.data && !Number.isNaN(parseFloat(response.data.weightLB))) {
+          return response.data.weightLB
+        } else {
+          return Promise.reject('Invalid response object')
+        }
+      })
+      .catch( (err) => {
+        throw new Error(err)
+      })
+  }
 }
--- tests/product-service.test.js
+++ tests/product-service.test.js
@@ -17,4 +17,21 @@ describe('Product service', function () {
         let weight = await productService.getProductWeight('13')
         expect(weight).toBe(15.5)
     })
+
+    it('Should handle unexpected response structure', async function () {
+        nock('https://mycluster.icp:8899/products')
+            .get('/19')
+            .reply(200, {
+              res: 15.5
+            })
+      
+        await productService
+            .getProductWeight('19')
+            .then(() => {
+              throw(new Error('Should not resolve in case of malformed data'))
+            })
+            .catch(err => {
+              expect(err.message).toBe('Invalid response object')
+            })
+        })
 })
